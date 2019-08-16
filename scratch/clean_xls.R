5229ec1b09ef18233ba38b988bc176a11492076c
Perkins <Genevieve.Perkins@gov.bc.ca>
2019-08-16 13:09:15 -0700

    new computer test

--- scratch/clean_xls.R
+++ scratch/clean_xls.R
@@ -10,6 +10,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 
+remotes::install_github("bcgov/bcgovr")
+remotes::install_github("bcgov/envreportutils")
+library(bcgovr)
+
 library(readxl)
 library(cellranger) # letter_to_num
 library(dplyr)
@@ -18,6 +22,7 @@ library(envreportutils)
 library(stringr)
 library(purrr)
 
+R.home()
 
 #excel_file <- file.path(
 #  soe_path("Operations ORCS/Data - Working/plants_animals/trends-status-native-species/2019"),
