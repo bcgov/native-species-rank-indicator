19d933766686a127510ac0b49fc262047e079dcb
GPerkins <Genevieve.Perkins@gov.bc.ca>
2019-09-26 13:15:14 -0700

    generate table of rank change per group

--- scratch/format_base_tables.R
+++ scratch/format_base_tables.R
@@ -82,41 +82,79 @@ group.oi <- c("Odonata", "Lepidoptera", "Molluscs")
 
 for(i in group.oi) {
 
- i = group.oi[2]
+  gref <- ref %>%
+    filter(Taxonomic_Group ==  i) %>%
+    select(ELCODE, Scientific_name, Year, Prov_Status) %>%
+    spread(Year, Prov_Status) %>%
+    mutate(Scientific_name = tolower(Scientific_name)) %>%
+    distinct()
 
-gref <- ref %>%
-  filter(Taxonomic_Group ==  i) %>%
-  select(ELCODE, Scientific_name, Year, Prov_Status) %>%
-  spread(Year, Prov_Status) %>%
-  distinct()
+  # read in the per 2004 data
+  pre2004 <- read_excel(file.path("data", "Compiled_pre2004",
+                                  paste(i ,"_pre2004.xlsx",sep = "")))
+  pre2004 <- pre2004 %>%
+    mutate(Scientific_name = tolower(Scientific_name))
 
+  data_all <- full_join(gref, pre2004)
 
-# read in the per 2004 data
-pre2004 <- read_excel(file.path("data", "Compiled_pre2004",
-                                 paste(i ,"_pre2004.xlsx",sep = "")))
+  # get the years of interest
+  oyears1 <- names(data_all)[str_detect(names(data_all), c("1"))]
+  oyears2 <- names(data_all)[str_detect(names(data_all), c("2"))]
+  oyears <- unique(c(oyears1, oyears2))
+  oyears <- as.list(as.character(sort(as.numeric(oyears))))
 
-data_all <- full_join(gref, pre2004) %>%
-  select(ELCODE, everything())
+  # create the new columns using fnc and add to data frame
+  new.cols.to.add <- as.vector(unlist(map(oyears, col.names.fn)))
 
-# get the years of interest
-oyears1 <- names(data_all)[str_detect(names(data_all), c("1"))]
-oyears2 <- names(data_all)[str_detect(names(data_all), c("2"))]
-oyears <- unique(c(oyears1, oyears2))
-oyears <- as.list(as.character(sort(as.numeric(oyears))))
+  data_all[, new.cols.to.add] <- ""
 
-# create the new columns using fnc and add to data frame
-new.cols.to.add <- as.vector(unlist(map(oyears, col.names.fn)))
+  groups.to.keep <- c("IIODO", "ILEP", "IMBIV")
 
-data_all[, new.cols.to.add] <- NA
+  data_all <- data_all %>%
+    filter(str_detect(ELCODE, paste(groups.to.keep , collapse = "|"))) %>%
+    select(sort(names(data_all))) %>%
+    select(ELCODE, Scientific_name, everything())
 
-groups.to.keep <- c("IIODO", "ILEP", "IMBIV")
-
-data_all <- data_all %>%
-  filter(str_detect(ELCODE, paste(groups.to.keep , collapse = "|")))
-
-write.csv(data_all, file.path("data",paste0(i, "_compiled.csv",sep = "")), row.names = FALSE)
+  write.csv(data_all, file.path("data",paste0(i, "_deliverable.csv",sep = "")), row.names = FALSE)
 
 }
 
 
+# create a data set for each group with change reasons
+
+# or read Git local version
+excel_file <- file.path("data",
+                        "Copy of Rank_Changes_Verts_Leps_Odonates_Molluscs2.xlsx")
+
+sdata <- read_excel(excel_file, sheet = "Query Output",
+                    range = "A2:O2731",
+                    col_types = c("numeric", "text", "numeric",
+                                  rep("text", 4), rep("date",3),
+                                  rep("text", 2), "numeric",
+                                  rep("text", 2)),
+                    col_names = c("ID", "ELCODE", "EST_ID",
+                                  "Scientific_Name", "Common_Name",
+                                  "current_SRANK", "BC_LIST",
+                                  "rank_review_date", "rank_change_date",
+                                  "change_entry_date", "prev_SRank",
+                                  "new_SRank", "code", "reason", "comment"))  %>%
+  select(Scientific_Name, Common_Name, everything()) %>%
+  filter(! BC_LIST == "Exotic") %>%
+  filter(str_detect(ELCODE, 'I')) %>%
+  filter(!str_detect(ELCODE, 'IMGAS'))
+
+leps <- sdata %>%
+  filter(str_detect(ELCODE, 'IMBIV'))
+
+write.csv(leps, file.path("data", "Contractor_datasets", "Lepidoptera",
+                          "Lepidoptera_Rank_change_reason.csv"))
+
+odo <- sdata
+
+  gref <- ref %>%
+    filter(Taxonomic_Group ==  i) %>%
+    select(ELCODE, Scientific_name, Year, Prov_Status) %>%
+    spread(Year, Prov_Status) %>%
+    mutate(Scientific_name = tolower(Scientific_name)) %>%
+    distinct()
 
