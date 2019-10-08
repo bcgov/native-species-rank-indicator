e91c1a775c7832400f65c707c4a52a41665258bf
GPerkins <Genevieve.Perkins@gov.bc.ca>
2019-10-08 14:45:37 -0700

    read in formatted data

--- 01_load.R
+++ 01_load.R
@@ -10,3 +10,24 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 
+
+remotes::install_github("bcgov/ranktrends")
+
+library(dplyr)
+library(ranktrends) # currently stored on 00_functions_temp_only.R
+
+
+
+# read in data set from data catalogue
+
+ref.0  <- read_csv("https://catalogue.data.gov.bc.ca/dataset/bc-vertebrates-conservation-status-rank-history-1992-2012/resource/842bcf0f-acd2-4587-a6df-843ba33ec271https://catalogue.data.gov.bc.ca/dataset/4484d4cd-3219-4e18-9a2d-4766fe25a36e/resource/842bcf0f-acd2-4587-a6df-843ba33ec271/download/historicalranksvertebrates1992-2012.csv")
+
+# or read in a local copy temporarily
+
+
+vdata <- readRDS(file.path("data","indata.r"))
+
+
+
+
+
