a1a2b26a0e3fb80c9e14898301fa7e002a24c0c7
GPerkins <Genevieve.Perkins@gov.bc.ca>
2020-01-17 17:04:12 -0800

    update for lowercase dataset

--- 03_analysis.R
+++ 03_analysis.R
@@ -11,44 +11,49 @@
 # See the License for the specific language governing permissions and limitations under the License.
 
 
+remotes::install_github("bcgov/ranktrends")
+
+library(ranktrends)
+
+
 # or read in a local copy temporarily
 indata <- readRDS(file.path("data","indata.r"))
 
 # note this is native sp.only
 
 status_data_wts <- indata %>%
-  mutate(parsed_rank_single = ranks_to_numeric(SRank, simplify = TRUE, round_fun = min),  # tried to extract 1st value ? not working
+  mutate(parsed_rank_single = ranks_to_numeric(rank, simplify = TRUE, round_fun = min),  # tried to extract 1st value ? not working
          wts = 5 - parsed_rank_single)
 
 status_complete <- status_data_wts %>%
-  group_by(Taxonomic_Group) %>%
-  complete(nesting(Scientific_Name, Common_name), Year) %>%
+  group_by(taxonomic_group) %>%
+  complete(nesting(scientific_name, common_name), year) %>%
   semi_join(
-    group_by(., Taxonomic_Group, Scientific_Name, Common_name) %>%
+    group_by(., taxonomic_group, scientific_name, common_name) %>%
       summarize())
 
 # remove those species which are extinct
 species_to_remove <- status_complete %>%
-  filter(Year == min(Year) & SRank == "SX") %>%
-  pull(Scientific_Name) %>%
+  filter(year == min(year) & rank == "SX") %>%
+  pull(scientific_name) %>%
   unique()
 
 
 status_data_final <- status_complete  %>%
-  filter(!Scientific_Name %in% species_to_remove) %>%
+  filter(!scientific_name %in% species_to_remove) %>%
   ungroup() %>%
-  mutate(Taxonomic_Group = ifelse(
-    Taxonomic_Group %in% c("Amphibians", "Reptiles and Turtles"),
+  mutate(taxonomic_group = ifelse(
+    taxonomic_group %in% c("Amphibians", "Reptiles and Turtles"),
     "Reptiles & Amphibians",
-    Taxonomic_Group))
+    taxonomic_group))
 
 # remove NAs
 status_data_final <- status_data_final %>%
   filter(!is.na(wts))
 
 #run function
-csi <- sampled_index(status_data_final, "Taxonomic_Group","wts","Year")
-csi <- csi %>% drop_na(Taxonomic_Group)
+csi <- sampled_index(status_data_final, "taxonomic_group","wts","year")
+csi <- csi %>% drop_na(taxonomic_group)
 
 
 saveRDS(csi, file.path("data","csi_taxonomic.R"))
@@ -57,34 +62,34 @@ saveRDS(csi, file.path("data","csi_taxonomic.R"))
 # Group by BC Listing  ----------------------------------------------------
 
 status_complete <- status_data_wts %>%
-  group_by(`BC List`) %>%
-  complete(nesting(Scientific_Name, Common_name), Year) %>%
+  group_by(bc_list) %>%
+  complete(nesting(scientific_name, common_name), year) %>%
   semi_join(
-    group_by(., `BC List`, Scientific_Name, Common_name) %>%
+    group_by(., bc_list, scientific_name, common_name) %>%
       summarize())
 
 # remove those species which are extinct
 species_to_remove <- status_complete %>%
-  filter(Year == min(Year) & SRank == "SX") %>%
-  pull(Scientific_Name) %>%
+  filter(year == min(year) & rank == "SX") %>%
+  pull(scientific_name) %>%
   unique()
 
 
 status_data_final <- status_complete  %>%
-  filter(!Scientific_Name %in% species_to_remove) %>%
+  filter(!scientific_name %in% species_to_remove) %>%
   ungroup() #%>%
-#  mutate(Taxonomic_Group = ifelse(
-#    Taxonomic_Group %in% c("Amphibians", "Reptiles and Turtles"),
+#  mutate(taxonomic_group = ifelse(
+#    taxonomic_group %in% c("Amphibians", "Reptiles and Turtles"),
 #    "Reptiles & Amphibians",
-#    Taxonomic_Group))
+#    taxonomic_group))
 
 # remove NAs
 status_data_final <- status_data_final %>%
   filter(!is.na(wts))
 
 #run function
-csi_bc <- sampled_index(status_data_final, "`BC List`","wts","Year")
-csi_bc <- csi_bc %>% drop_na(`BC List`)
+csi_bc <- sampled_index(status_data_final, "bc_list","wts","year")
+csi_bc <- csi_bc %>% drop_na(bc_list)
 
 
 saveRDS(csi_bc, file.path("data","csi_bclist.R"))
