ecad3bc967c6b4f85f59400879b59812189a4ceb
GPerkins <Genevieve.Perkins@gov.bc.ca>
2020-04-15 20:37:04 -0700

    join provinical status information

--- 01_load_clean.R
+++ 01_load_clean.R
@@ -13,14 +13,51 @@
 
 # remotes::install_github("bcgov/ranktrends")
 
+library(readr)
 library(dplyr)
 library(ranktrends)
 
 # read in data set from data catalogue
 #ref.0  <- read_csv("https://catalogue.data.gov.bc.ca/dataset/bc-vertebrates-conservation-status-rank-history-1992-2012/resource/842bcf0f-acd2-4587-a6df-843ba33ec271https://catalogue.data.gov.bc.ca/dataset/4484d4cd-3219-4e18-9a2d-4766fe25a36e/resource/842bcf0f-acd2-4587-a6df-843ba33ec271/download/historicalranksvertebrates1992-2012.csv")
 
+
 # or read in a local copy temporarily
 indata <- readRDS(file.path("data","indata.r"))
+
+indata <- read_csv(file.path("data", "inverts_consolidated.csv"))
+
+
+
+provincial_listing <-
+  new.data <- file.path("data","raw",
+                        "BCSEE_Plants_Animals_final.csv")
+
+  new.0 <- read_csv(file.path("data","raw",
+                              "BCSEE_Plants_Animals_final.csv"),
+                    col_types = cols_only(
+                      Year = col_integer(),
+                      `Scientific Name` = col_character(),
+                      `English Name` = col_character(),
+                      `Element Code` = col_character(),
+                      `Name Category` = col_character(),
+                      `BC List` = col_character(),
+                      `Origin` = col_character()
+                    )
+  ) %>%
+    rename_all(function(x) tolower(gsub("\\s+", "_", x))) %>%
+    filter(!name_category %in% c("Vascular Plant", "Non-Vascular Plant",
+                                 "Nonvascular Plant", "Fungus",
+                                 "International Vegetation Classification")) %>%
+    group_by(scientific_name) %>%
+    filter(year == max(year)) %>%
+    ungroup() %>%
+    select(-c(year, english_name, name_category))
+
+
+indata <- indata %>%
+  left_join()
+
+
 
 
 # check the number of species by types:
