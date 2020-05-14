ab0dc4a78f30fad65f437e87f57f998eb381efd9
GPerkins <Genevieve.Perkins@gov.bc.ca>
2020-05-14 12:17:15 -0700

    generate table of assessment yrs per tax group

--- 02_clean.R
+++ 02_clean.R
@@ -12,14 +12,64 @@
 
 
 
-# place holder to generate the time periods of interest based on taxonomic groups.
-
-
+#devtools::install_github("tidyverse/dplyr")
 library(dplyr)
 library(ranktrends)
 library(tidyr)
-
+library(ggplot2)
 
 # or read in a local copy temporarily
 indata <- readRDS(file.path("data","indata.r"))
+
+
+# filter species to remove (exotics)
+
+indata <- indata %>%
+  filter(!origin %in% c("Exotic", "Unknown/Undetermined")) %>%
+  filter(!bc_list == "accidental")
+
+
+# check the number of species by BC List
+#sp.catergory <- indata %>%
+#  group_by(origin, bc_list) %>%
+#  summarise(count = n())
+
+
+# get a summary of species per taxanomic group
+
+tax_sum <- indata %>%
+  group_by(taxonomic_group)%>%
+  summarise(across(scientific_name, n_distinct))
+
+
+# calculate the time points for each group assessment and make a table
+
+yrs <- indata %>%
+  filter(!is.na(srank))
+
+ggplot(yrs, aes(year)) +
+  geom_bar() +
+  facet_wrap(vars(taxonomic_group), scales = "free")
+
+
+# generate a table
+yrs_tax <- indata %>%
+  filter(!is.na(srank)) %>%
+  group_by(taxonomic_group, year) %>%
+  summarise(count = n()) %>%
+  ungroup() %>%
+  left_join(tax_sum) %>%
+  filter(count >= scientific_name/2)  # keep years with more than 50% sp ranked (?)
+
+
+
+# AT : this is up for discussion?
+# note alternate could be to fill in years and ranks up to the last rank date?
+
+
+# write R objects
+saveRDS(indata, file = "data/indata.rds")
+saveRDS(yrs_tax, file = "data/yrs_tax.rds")
+
+
 
